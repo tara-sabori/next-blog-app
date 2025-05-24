3815561548ace930497557a6b97c6b9b7e325b5e
tara-sabori <tarasaboori@gmail.com>
2025-05-24 19:57:31 +0330

    add initial config files and assets

---
+++ frontend/next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol:'http',
+                hostname:'localhost',
+                port:'5000',
+                pathname:'/uploads/**'
+            }
+        ]
+    },
+    // experimental:{
+    //     ppr:'incremental'
+    // }
+};
+
+export default nextConfig;
